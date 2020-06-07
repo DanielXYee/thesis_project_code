b225c6b3435470bb7041f54c78b399632af6c154
DanielXYee <xianyong.yi@kaust.edu.sa>
2020-06-07 11:36:20 +0000

    calculate feature count

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,6 +7,6 @@
 # new environment variables
 
 # modified environment variables
-export PKG_CONFIG_PATH="/home/im_hongxian_yi/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="/home/im_hongxian_yi/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
+export PKG_CONFIG_PATH="/home/im_hongxian_yi/catkin_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/im_hongxian_yi/catkin_ws/build"
-export PYTHONPATH="/home/im_hongxian_yi/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
